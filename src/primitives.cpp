66367958e6e4be56cb170f0dee48b02d2a3f53f6
Laurent Valette <lau.valette@gmail.com>
2014-06-19 18:14:30 +0200

    Add Circle, Cube and Triangle primitives

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -1,10 +1,6 @@
 #include "primitives.hpp"
 
-Primitive::Primitive(): m_color(0.f), m_light(false)
-{
-}
-
-Primitive::Primitive(glm::vec4 color, bool isLight): m_color(color), m_light(isLight)
+Primitive::Primitive(const glm::vec4 color, const bool isLight): m_color(color), m_light(isLight)
 {
 }
 
@@ -26,4 +22,32 @@ bool Primitive::isLight() const
 void Primitive::setLight(const bool isLight)
 {
 	m_light = isLight;
+}
+
+Sphere::Sphere(const glm::vec4 color, const bool isLight, const glm::vec3 center, const float radius):
+			Primitive(color, isLight), m_center(center), m_radius(radius)
+{
+}
+
+Cube::Cube(const glm::vec4 color, const bool isLight, const glm::vec3 origin, const glm::vec3 width, const glm::vec3 length, const glm::vec3 height):
+			Primitive(color, isLight), m_origin(origin), m_width(width), m_length(length), m_height(height)
+{
+}
+
+Triangle::Triangle(const glm::vec4 color, const bool isLight, const glm::vec3 a, const glm::vec3 b, const glm::vec3 c): Primitive(color, isLight), m_a(a); m_b(b), m_c(c)
+{
+}
+
+Ray::Ray(const glm::vec3 origin, const glm::vec3 direction): m_origin(origin), m_direction(direction)
+{
+}
+
+glm::vec3 Ray::getOrigin() const
+{
+	return m_origin;
+}
+
+glm::vec3 Ray::getDirection() const
+{
+	return m_direction;
 }
--- src/primitives.hpp
+++ src/primitives.hpp
@@ -8,17 +8,58 @@
 class Primitive
 {
 public:
-	Primitive();
-	Primitive(glm::vec4 color, bool isLight);
+	Primitive(const glm::vec4 color, const bool isLight);
 
 	glm::vec4 getColor() const;
 	void setColor(const glm::vec4 color);
 	bool isLight() const;
 	void setLight(const bool isLight);
+	float intersect(const Ray ray) const;
 
-private:
+protected:
 	glm::vec4 m_color;
 	bool m_light;
+}
+
+class Sphere: public Primitive
+{
+public:
+	Sphere(const glm::vec4 color, const bool isLight, const glm::vec3 center, const float radius);
+
+private:
+	glm::vec3 m_center;
+	float m_radius;
+}
+
+// Actually, Cube defines a parallelepiped
+class Cube: public Primitive
+{
+public:
+	Cube(const glm::vec4 color, const bool isLight, const glm::vec3 origin, const glm::vec3 width, const glm::vec3 length, const glm::vec3 height);
+
+private:
+	glm::vec3 m_origin, m_width, m_length, m_height;
+}
+
+class Triangle: public Primitive
+{
+public:
+	Triangle(const glm::vec4 color, const bool isLight, const glm::vec3 a, const glm::vec3 b, const glm::vec3 c);
+
+private:
+	glm::vec3 m_a, m_b, m_c;
+}
+
+class Ray
+{
+public:
+	Ray(const glm::vec3 origin, const glm::vec3 direction);
+	glm::vec3 getOrigin() const;
+	glm::vec3 getDirection() const;
+
+private:
+	glm::vec3 m_origin;
+	glm::vec3 m_direction;
 }
 
 #endif
